40d23338c1d3a6359ce1bb8101b86b78b540de4d
teambena <teetanic27@gmail.com>
2022-05-31 19:28:18 +0700

    Can filter the customer through the current manager customer.
    add the customer selection of the manager in the add appointment.

--- app/controllers/components_data.py
+++ app/controllers/components_data.py
@@ -6,6 +6,20 @@ from app import *
 Components_Data_blueprint = Blueprint('components_data', __name__)
 
 
+
+
+@Components_Data_blueprint.route('/customer_option_list')
+@jwt_required(optional=True)
+def customer_option_list():
+    try:
+        sqltext = text(f"""SELECT customer_name AS value,customer_name AS label FROM Customer WHERE uid=:manager_id""")
+        query_params = dict()
+        query_params['manager_id'] = current_user.manager_id;
+        arr = db.session.execute(sqltext, query_params)
+        
+        return jsonify([dict(row) for row in arr])
+    except Exception as ex:
+        return InternalServerError(ex)
 from ..models.manager import Manager
 
 
