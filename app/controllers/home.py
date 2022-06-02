49f1a1e4314e496f9e727393200c3d07d137b3c2
teambena <teetanic27@gmail.com>
2022-06-02 15:18:10 +0700

    make add calendar api.

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -9,4 +9,9 @@ Home_blueprint = Blueprint('Home', __name__)
 @Home_blueprint.route('/index')
 @jwt_required()
 def home():
-    return render_template('pages/home/index.html')
+    user_role = current_user.get_role()
+    pages = ['calendar']
+    if user_role in pages:
+        return render_template('pages/home/{0}.html'.format(user_role))
+    else:
+        return render_template('pages/home/index.html')
