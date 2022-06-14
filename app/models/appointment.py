9f7572f37dac0970a88c8f0b2bf54f4b93a454cb
teambenq <teetanic27@gmail.com>
2022-06-14 20:44:58 +0700

    Change datatype of date and status.

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -1,19 +1,19 @@
 from app import db,  OptionalButNotEmpty, datetime, timedelta, config
-from wtforms import Form, StringField, FloatField, IntegerField, BooleanField
+from wtforms import Form, StringField, FloatField, IntegerField, BooleanField, DateField
 from wtforms.validators import *
 
 
 class Appointment(db.Model):
     __tablename__ = 'Appointment'
     customer = db.Column("customer", db.String)
-    start_date = db.Column("start_date", db.String)
+    start_date = db.Column("start_date", db.DATE)
     start_time = db.Column("start_time", db.String)
-    end_date = db.Column("end_date", db.String)
+    end_date = db.Column("end_date", db.DATE)
     end_time = db.Column("end_time", db.String)
     description = db.Column("description", db.String)
     uid = db.Column("uid", db.String)
+    status = db.Column("status", db.String)
     appointment_id = db.Column("appointment_id", db.Integer, primary_key=True, autoincrement=True)
-    status = db.Column("status", db.Float)
     
     def _asdict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
@@ -21,20 +21,20 @@ class Appointment(db.Model):
 
 class AppointmentAddForm(Form):
     customer = StringField('Customer', [])
-    start_date = StringField('Start Date', [])
+    start_date = DateField('Start Date', format='%Y-%m-%d')
     start_time = StringField('Start Time', [])
-    end_date = StringField('End Date', [])
+    end_date = DateField('End Date', format='%Y-%m-%d')
     end_time = StringField('End Time', [])
     description = StringField('Description', [])
     uid = StringField('Uid', [])
-    status = FloatField('Status', [NumberRange()])
+    status = StringField('Status', [])
 
 
 class AppointmentEditForm(Form):
     customer = StringField('Customer', [])
-    start_date = StringField('Start Date', [])
+    start_date = DateField('Start Date', format='%Y-%m-%d')
     start_time = StringField('Start Time', [])
-    end_date = StringField('End Date', [])
+    end_date = DateField('End Date', format='%Y-%m-%d')
     end_time = StringField('End Time', [])
     description = StringField('Description', [])
-    status = FloatField('Status', [NumberRange()])
+    status = StringField('Status', [])
